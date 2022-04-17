23ee745e936b205ea2383bf11c3a5d80e6888284
Htyg <oleg.kanakaev@gmail.com>
2022-04-17 23:38:35 +0500

    oeu

---
+++ taxi/taxi/Program.cs
@@ -0,0 +1,13 @@
+﻿int nomber;
+string c;
+for(int i = 0; i < 100000; i++)
+{
+    c = Convert.ToString(i);
+    nomber = c.Length;
+    while (nomber < 5)
+    {
+        Console.Write("0");
+        nomber++;
+    }
+    Console.WriteLine(i);
+}
